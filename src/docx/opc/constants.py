798209b924abefaed315da3e3931433e1267ffd6
coderamaster <dillon.work323@gmail.com>
2026-01-21 10:04:24 -0500

    Feature: Add support for .dotx Word templates (#1532) - Added WML_TEMPLATE_MAIN content type constant - Updated Document() to accept both .docx and .dotx files - Added part factory mapping for template content type - Added tests for .dotx template loading - Fixes #1532

--- src/docx/opc/constants.py
+++ src/docx/opc/constants.py
@@ -135,6 +135,9 @@ class CONTENT_TYPE:
     WML_DOCUMENT_MAIN = (
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"
     )
+    WML_TEMPLATE_MAIN = (
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.template.main+xml"
+    )
     WML_ENDNOTES = "application/vnd.openxmlformats-officedocument.wordprocessingml.endnotes+xml"
     WML_FONT_TABLE = "application/vnd.openxmlformats-officedocument.wordprocessingml.fontTable+xml"
     WML_FOOTER = "application/vnd.openxmlformats-officedocument.wordprocessingml.footer+xml"
